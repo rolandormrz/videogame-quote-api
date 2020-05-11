e535e234c88e71524d0d4a831d4059daacea52ab
Rolando Ramirez <rolandormrz@yahoo.com>
2020-05-11 18:11:28 -0400

    refactor html files and deploy to heroku

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const quotesRouter = require('./controllers/quotes');
 const middleware = require('./utils/middleware');
 
+app.use(cors());
 app.use(express.static('public'));
 app.use(express.json());
 app.use(middleware.requestLogger);
